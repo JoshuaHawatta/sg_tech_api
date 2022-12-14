842ad9ea8dda92e4c0364b946de5d3433fdd582f
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-14 16:49:23 -0300

    fix: fixed the missing <type> property for each Schema field at ClientSchema.ts

--- src/Client/ClientSchema.ts
+++ src/Client/ClientSchema.ts
@@ -6,14 +6,14 @@ const ClientSchema = model(
 	'Client',
 
 	new Schema<IClient>({
-		name: { String, required: true },
-		email: { String, required: true },
-		phone: { Number, required: true },
-		password: { String, required: true },
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		phone: { type: Number, required: true },
+		password: { type: String, required: true },
 		image: String,
 		my_appointments: Object,
-		createdAt: { Date, required: true, immutable: true, default: generateDate },
-		updatedAt: { Date, required: true, default: generateDate },
+		createdAt: { type: Date, required: true, immutable: true, default: generateDate },
+		updatedAt: { type: Date, required: true, default: generateDate },
 	})
 )
 
