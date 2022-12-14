10afe86b44e905ebbca8358209e8115dd8ad7d85
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-14 17:57:21 -0300

    implements: Added more validations to the registerAccount method at ClientController.ts

--- src/Client/ClientController.ts
+++ src/Client/ClientController.ts
@@ -6,15 +6,26 @@ export default class ClientController {
 		const { name, email, phone, password, confirmPassword } = req.body
 
 		if (!name) return res.status(422).json({ message: 'Nome obrigatório!' })
-		if (!email) return res.status(422).json({ message: 'E-mail obrigatório!' })
-		if (!phone) return res.status(422).json({ message: 'Telefone obrigatório!' })
-		if (!password) return res.status(422).json({ message: 'Senha obrigatória!' })
-		if (!confirmPassword) return res.status(422).json({ message: 'Confirme sua senha!' })
-		if (confirmPassword !== password)
+		else if (!email) return res.status(422).json({ message: 'E-mail obrigatório!' })
+		else if (!phone) return res.status(422).json({ message: 'Telefone obrigatório!' })
+		else if (!password) return res.status(422).json({ message: 'Senha obrigatória!' })
+		else if (!confirmPassword) return res.status(422).json({ message: 'Confirme sua senha!' })
+		else if (confirmPassword !== password)
 			return res.status(422).json({ message: 'As senhas não estão iguais!' })
 
-		const client = await (await ClientSchema.create({ name, email, phone, password })).save()
+		const existClient = await ClientSchema.find({ email: email }).select('-password')
 
-		return res.status(201).json({ message: 'Conta criada com sucesso!', client })
+		if (existClient.length > 0)
+			return res.status(422).json({ message: 'Este e-mail já está em uso!' })
+
+		const client = new ClientSchema({ name, email, phone, password })
+
+		try {
+			const signClient = await client.save()
+
+			return res.status(201).json({ message: 'Conta criada com sucesso!', signClient })
+		} catch (err) {
+			return res.status(500).json({ message: 'Não coseguimos realizar seu cadastro no momento.' })
+		}
 	}
 }
