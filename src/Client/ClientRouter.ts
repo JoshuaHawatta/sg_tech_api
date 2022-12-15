36041de46d0ae61fb37e54dcd5ac7c9a53712495
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-15 00:30:51 -0300

    chore: added the <verifyToken> middleware at the 'checkclient' route at ClientRouter.ts

--- src/Client/ClientRouter.ts
+++ src/Client/ClientRouter.ts
@@ -1,11 +1,12 @@
 import Router from 'express'
 import ClientController from './ClientController'
+import verifyToken from '../middlewares/verify-token'
 
 const clientRouter = Router()
 
 clientRouter.post('/register', ClientController.registerAccount)
 clientRouter.post('/login', ClientController.login)
 
-clientRouter.get('/checkclient', ClientController.checkLoggedClient)
+clientRouter.get('/checkclient', verifyToken, ClientController.checkLoggedClient)
 
 export default clientRouter
