ed5eb93aa35dabad8171d0d087c57ace4054085d
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-16 17:51:36 -0300

    implements: created a class to substitute  all existing JWT helper functions

---
+++ src/helpers/Jwt-token-handler.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import { Types } from 'mongoose'
+
+type TClient = {
+	_id: Types.ObjectId | string
+	name: string
+}
+
+export default class JwtTokenHandler {
+	static generateToken(client: TClient, res: Response) {
+		const newToken = jwt.sign(
+			{ name: client.name, _id: client._id },
+			process.env.API_SECRET as string,
+
+			{ expiresIn: '4 hours' }
+		)
+
+		return res.status(200).json({ message: 'Bem vindo(a) ao sistema!', newToken, _id: client._id })
+	}
+
+	static async getToken(req: Request, res: Response) {
+		const authHeader = req.headers.authorization
+
+		if (!authHeader) res.status(401).json({ message: 'Acesso negado!' })
+
+		return authHeader?.split(' ')[1] ?? 'INVALID_CLIENT_TOKEN'
+	}
+
+	static async getClientByToken(req: Request, res: Response) {
+		const token = await JwtTokenHandler.getToken(req, res)
+
+		return jwt.verify(token, process.env.API_SECRET as string) as TClient
+	}
+
+	static async verifyToken(req: Request, res: Response, next: NextFunction) {
+		const token = await JwtTokenHandler.getToken(req, res)
+
+		try {
+			const decodedClient = jwt.verify(token, process.env.API_SECRET as string)
+			console.log(decodedClient)
+
+			return next()
+		} catch (err) {
+			return res.status(500).json({ message: 'Token inválido!' })
+		}
+	}
+}
