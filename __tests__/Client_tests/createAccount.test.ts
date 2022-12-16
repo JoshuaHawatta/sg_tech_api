ea2f86b6539c68743518bc38c51670528951856d
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-16 00:18:40 -0300

    chore: Finished the 'happy path' to create an account at createAccount.test.ts

--- __tests__/Client_tests/createAccount.test.ts
+++ __tests__/Client_tests/createAccount.test.ts
@@ -1,61 +1,95 @@
 import ClientSchema from '../../src/Client/ClientSchema'
 import generateDate from '../../src/helpers/generate-date'
-import FakeClientController from '../../src/fakes/FakeClientController'
+import { Request, Response } from 'express'
+import ClientController from '../../src/Client/ClientController'
 
+jest.mock('../../src/Client/ClientController')
+
+let req: Request
+let res: Response
+
+type TClient = {
+	name: string
+	email: string
+	password: string
+	createdAt: Date
+	updatedAt: Date
+}
+
+//HAPPY_PATH
 describe('Client should be able to create account when...', () => {
 	afterEach(() => jest.clearAllMocks())
 
 	test('all fields are are filled', async () => {
-		ClientSchema.findOne = jest.fn().mockReturnValueOnce({
-			name: 'Emiliano Azevedo',
-			email: 'emilianoazevedo@gmail.com',
-			password: 'ivorycoast1972',
+		ClientController.registerAccount = jest.fn().mockImplementationOnce(() => ({
+			name: 'randomUser',
+			email: 'randomuser@gmail.com',
+			password: 'randompassword',
 			createdAt: generateDate(),
 			updatedAt: generateDate(),
-		})
+		}))
 
-		const fakeClient = await ClientSchema.findOne()
-		const confirmPassword = 'ivorycoast1972'
+		const mockedNewUser = await ClientController.registerAccount(req, res)
+		const confirmPassword = 'randompassword'
 
-		expect(fakeClient).toHaveProperty('name')
-		expect(fakeClient).toHaveProperty('email')
-		expect(fakeClient).toHaveProperty('password')
+		const mockedUserToInsert = new ClientSchema(mockedNewUser)
+
+		expect(mockedNewUser).toHaveProperty('name')
+		expect(mockedNewUser).toHaveProperty('email')
+		expect(mockedNewUser).toHaveProperty('password')
 		expect(confirmPassword).not.toBe('')
-		expect(fakeClient).toHaveProperty('createdAt')
-		expect(fakeClient).toHaveProperty('updatedAt')
+		expect(mockedNewUser).toHaveProperty('createdAt')
+		expect(mockedNewUser).toHaveProperty('updatedAt')
+
+		mockedUserToInsert.save = jest.fn().mockImplementation(() => ({}))
+		mockedUserToInsert.save()
 	})
 
 	test('when passwords are equal', async () => {
-		ClientSchema.findOne = jest.fn().mockReturnValueOnce({
-			name: 'Emiliano Azevedo',
-			email: 'emilianoazevedo@gmail.com',
-			password: 'ivorycoast1972',
+		ClientController.registerAccount = jest.fn().mockImplementationOnce(() => ({
+			name: 'randomUser',
+			email: 'randomuser@gmail.com',
+			password: 'randompassword',
 			createdAt: generateDate(),
 			updatedAt: generateDate(),
-		})
+		}))
 
-		const fakeClient = await ClientSchema.findOne()
-		const confirmPassword = 'ivorycoast1972'
+		const mockedNewUser = (await ClientController.registerAccount(req, res)) as unknown as TClient
+		const confirmPassword = 'randompassword'
 
-		expect(fakeClient).toHaveProperty('name')
-		expect(fakeClient).toHaveProperty('email')
-		expect(fakeClient).toHaveProperty('password')
+		expect(mockedNewUser).toHaveProperty('name')
+		expect(mockedNewUser).toHaveProperty('email')
+		expect(mockedNewUser).toHaveProperty('password')
 		expect(confirmPassword).not.toBe('')
-		expect(fakeClient?.password).toStrictEqual(confirmPassword)
-		expect(fakeClient).toHaveProperty('createdAt')
-		expect(fakeClient).toHaveProperty('updatedAt')
+		expect(confirmPassword).toStrictEqual(mockedNewUser.password)
+		expect(mockedNewUser).toHaveProperty('createdAt')
+		expect(mockedNewUser).toHaveProperty('updatedAt')
+
+		const mockedUserToInsert = new ClientSchema(mockedNewUser)
+
+		mockedUserToInsert.save = jest.fn().mockImplementation(() => ({}))
+		mockedUserToInsert.save()
 	})
 
-	test('when the email is not beeing used', async () => {
-		ClientSchema.findOne = jest.fn().mockReturnValueOnce({ email: 'emilianoazevedo@gmail.com' })
-		const clientController = new FakeClientController()
-
-		const mockedRegisteredClient = await ClientSchema.findOne()
-
-		const newClientEmail = await clientController.registerAccount({
-			email: 'newUser@gmail.com',
+	test('Has no user registered with choosen e-mail', async () => {
+		ClientSchema.findOne = jest.fn().mockReturnValueOnce({
+			name: 'randomUser',
+			email: 'mockeduser@gmail.com',
 		})
 
-		expect(newClientEmail).not.toStrictEqual(mockedRegisteredClient)
+		ClientController.registerAccount = jest
+			.fn()
+			.mockImplementationOnce(() => ({ name: 'randomUser', email: 'randomuser@email.com' }))
+
+		const mockedDBUser = await ClientSchema.findOne()
+		const signedUser = await ClientController.registerAccount(req, res)
+
+		expect(signedUser).not.toStrictEqual(mockedDBUser)
 	})
+})
+
+//UNHAPPY_PATH
+describe('Client should NOT be able to create account when...', () => {
+	afterEach(() => jest.clearAllMocks())
+	//CREATE_TESTS_FOR_THE_'FAILING'_ATTEMPTS...
 })
