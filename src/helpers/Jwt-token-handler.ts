5592081012b8bf1f8c6da792c1ccfe05915e1709
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-18 13:34:55 -0300

    chore: added a return type for each method at Jwt-token-handler.ts

--- src/helpers/Jwt-token-handler.ts
+++ src/helpers/Jwt-token-handler.ts
@@ -8,7 +8,7 @@ type TClient = {
 }
 
 export default class JwtTokenHandler {
-	static generateToken(client: TClient, res: Response) {
+	static generateToken(client: TClient, res: Response): Response {
 		const newToken = jwt.sign(
 			{ name: client.name, _id: client._id },
 			process.env.API_SECRET as string,
@@ -19,7 +19,7 @@ export default class JwtTokenHandler {
 		return res.status(200).json({ message: 'Bem vindo(a) ao sistema!', newToken, _id: client._id })
 	}
 
-	static async getToken(req: Request, res: Response) {
+	static async getToken(req: Request, res: Response): Promise<string> {
 		const authHeader = req.headers.authorization
 
 		if (!authHeader) res.status(401).json({ message: 'Acesso negado!' })
@@ -27,13 +27,13 @@ export default class JwtTokenHandler {
 		return authHeader?.split(' ')[1] ?? 'INVALID_CLIENT_TOKEN'
 	}
 
-	static async getClientByToken(req: Request, res: Response) {
+	static async getClientByToken(req: Request, res: Response): Promise<TClient> {
 		const token = await JwtTokenHandler.getToken(req, res)
 
-		return jwt.verify(token, process.env.API_SECRET as string) as TClient
+		return jwt.verify(token, process.env.API_SECRET as string) as Promise<TClient>
 	}
 
-	static async verifyToken(req: Request, res: Response, next: NextFunction) {
+	static async verifyToken(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
 		const token = await JwtTokenHandler.getToken(req, res)
 
 		try {
