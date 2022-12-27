e1704c9f0c957253c014526c6f02987e552f0a36
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2022-12-27 19:53:06 -0300

    fix: removed <if> condition at <getAllAppointments> method at AppointmentController.ts

--- src/Appointments/AppointmentController.ts
+++ src/Appointments/AppointmentController.ts
@@ -3,11 +3,8 @@ import JwtTokenHandler from '../helpers/Jwt-token-handler'
 import AppointmentSchema from './AppointmentSchema'
 
 export default class AppointmentController {
-	static async getClientAllAppointments(req: Request, res: Response) {
+	static async getAllAppointments(req: Request, res: Response) {
 		const loggedUser = await JwtTokenHandler.getUserByToken(req, res)
-
-		if (loggedUser.accesses.includes('Seller'))
-			res.status(401).json({ message: 'Você não pode ver agendamentos de clientes!' })
 
 		try {
 			const allAppointments = await AppointmentSchema.find({ 'client._id': loggedUser._id }).sort(
