8d0ff08c085295c36456ec499621e3b6372721d7
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-01-06 01:05:24 -0300

    chore: added users validations middlewares at UserRouter.ts

--- src/User/UserRouter.ts
+++ src/User/UserRouter.ts
@@ -1,22 +1,28 @@
 import Router from 'express'
 import UserController from './UserController'
-import JwtTokenHandler from '../helpers/Jwt-token-handler'
 import imageUpload from '../config/multer'
+
+//MIDDLEWARES
+import verifyToken from '../middlewares/verifyToken'
+import { login, loginResults } from '../middlewares/validations/user/login'
+import { createOrUpdate, createOrUpdateResults } from '../middlewares/validations/user/createOrUpdate'
 
 const userRouter = Router()
 
 userRouter.post('/register', UserController.registerAccount)
-userRouter.post('/login', UserController.login)
+userRouter.post('/login', login, loginResults, UserController.login)
 
 userRouter.patch(
 	'/updatedata',
-	JwtTokenHandler.verifyToken,
+	verifyToken,
+	createOrUpdate,
+	createOrUpdateResults,
 	imageUpload.single('image'),
 	UserController.updateAccountData
 )
 
-userRouter.delete('/deleteaccount', JwtTokenHandler.verifyToken, UserController.deleteAccount)
+userRouter.delete('/deleteaccount', verifyToken, UserController.deleteAccount)
 
-userRouter.get('/checkuser', JwtTokenHandler.verifyToken, UserController.checkLoggedUser)
+userRouter.get('/checkuser', verifyToken, UserController.checkLoggedUser)
 
 export default userRouter
