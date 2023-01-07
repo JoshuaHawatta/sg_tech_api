875f441107471d5fb6a72e6baed1c4a314c6ac03
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-01-07 00:47:46 -0300

    chore: minor change at database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false)
 
 const mongoDBConnect = async (): Promise<void> => {
 	try {
-		await mongoose.connect(`${process.env.MONGO_URL}`)
+		await mongoose.connect(process.env.MONGO_URL as string)
 		console.log('Conexão ao MongoDB Atlas gerada')
 	} catch (err) {
 		console.log('Conexão ao MongoDB Atlas mal-sucedida')
