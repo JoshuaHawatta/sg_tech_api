c6cdba49af653cfe051ad02e6ca841a1c962999d
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-01-07 00:47:29 -0300

    implements: added all validations for each route at AppointmentRouter.ts

--- src/Appointments/AppointmentRouter.ts
+++ src/Appointments/AppointmentRouter.ts
@@ -1,20 +1,40 @@
 import { Router } from 'express'
+
+//CONTROLLERS
 import GeneralController from './controllers/GeneralController'
 import AdminController from './controllers/AdminController'
+
+//MIDDLEWARES
 import verifyToken from '../middlewares/verifyToken'
+import finishService from '../middlewares/validations/appointments/finishService'
+import addAppointment from '../middlewares/validations/appointments/addAppointment'
+import getValidationsResults from '../middlewares/getValidationsResults'
 
 const appointmentRouter = Router()
 
 //ADMIN_ROUTES
-appointmentRouter.post('/addappointment', verifyToken, GeneralController.addAppointment)
-
 appointmentRouter.patch(
 	'/confirmappointment/:id',
 	verifyToken,
 	AdminController.confirmOrDeclineAppointment
 )
 
-appointmentRouter.patch('/finishservice/:id', verifyToken, AdminController.finishService)
+appointmentRouter.patch(
+	'/finishservice/:id',
+	verifyToken,
+	finishService,
+	getValidationsResults,
+	AdminController.finishService
+)
+
+//USER_ROUTES
+appointmentRouter.post(
+	'/addappointment',
+	verifyToken,
+	addAppointment,
+	getValidationsResults,
+	GeneralController.addAppointment
+)
 
 //GENERAL_ROUTES
 appointmentRouter.get('/allappointments/:id', verifyToken, GeneralController.getEspecificAppointment)
