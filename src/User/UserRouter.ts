f7847e9bc400765ba10dcb238c6b7283c0395ad4
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-01-07 00:43:43 -0300

    implements: added all validations at UserRoutes.ts

--- src/User/UserRouter.ts
+++ src/User/UserRouter.ts
@@ -4,20 +4,21 @@ import imageUpload from '../config/multer'
 
 //MIDDLEWARES
 import verifyToken from '../middlewares/verifyToken'
-import { login, loginResults } from '../middlewares/validations/user/login'
-import { createOrUpdate, createOrUpdateResults } from '../middlewares/validations/user/createOrUpdate'
+import login from '../middlewares/validations/user/login'
+import getValidationsResults from '../middlewares/getValidationsResults'
+import createOrUpdate from '../middlewares/validations/user/createOrUpdate'
 
 const userRouter = Router()
 
-userRouter.post('/register', UserController.registerAccount)
-userRouter.post('/login', login, loginResults, UserController.login)
+userRouter.post('/register', createOrUpdate, getValidationsResults, UserController.registerAccount)
+userRouter.post('/login', login, getValidationsResults, UserController.login)
 
 userRouter.patch(
 	'/updatedata',
 	verifyToken,
-	createOrUpdate,
-	createOrUpdateResults,
 	imageUpload.single('image'),
+	createOrUpdate,
+	getValidationsResults,
 	UserController.updateAccountData
 )
 
