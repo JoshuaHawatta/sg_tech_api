bfccc4f06837efc2d1b8e19aef4b954b47777e34
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-01-15 20:41:45 -0300

    fix: wrong <appointmentDate> property name bug at MailingHandler fixed

--- src/helpers/Mailing-handler.ts
+++ src/helpers/Mailing-handler.ts
@@ -6,14 +6,12 @@ const emailSender = process.env.NODE_MAILER_EMAIL as string
 
 export default class MailingHandler {
 	static appointmentEmail(to: string, appointmentContent: IAppointment): IEmailContent {
-		const { appointment_date, confirmedService } = appointmentContent
-
-		const appointmentDate =
-			moment(appointment_date).format('MM/MM/YYYY, hh:mm') ?? 'Data indisponível!'
+		const { appointmentDate, confirmedService } = appointmentContent
+		const choosedDate = moment(appointmentDate).format('MM/MM/YYYY, hh:mm') ?? 'Data indisponível!'
 
 		const text = confirmedService
-			? `Seu agendamento na data: ${appointmentDate} foi realizado com sucesso!`
-			: `Seu agendamento na data: ${appointmentDate} infelizmente precisou ser recusado!`
+			? `Seu agendamento na data: ${choosedDate} foi realizado com sucesso!`
+			: `Seu agendamento na data: ${choosedDate} infelizmente precisou ser recusado!`
 
 		const subject = confirmedService ? 'Agendamento confirmado!' : 'Agendamento recusado :('
 
