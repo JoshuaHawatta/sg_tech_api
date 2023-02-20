4017cb72012ca4f30791993a31d39ea791f79234
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-02-20 19:49:07 -0300

    chore: minor text changes at addAppointment.ts validations

--- src/middlewares/validations/appointments/addAppointment.ts
+++ src/middlewares/validations/appointments/addAppointment.ts
@@ -1,7 +1,7 @@
 import { body } from 'express-validator'
 
 const addAppointment = [
-	body('serviceType').trim().not().isEmpty().withMessage('Informe o tipo de serviço!!'),
+	body('serviceType').trim().not().isEmpty().withMessage('Informe o tipo de serviço!'),
 	body('appointmentDate').trim().not().isEmpty().withMessage('Data de agendamento obrigatória!'),
 ]
 
